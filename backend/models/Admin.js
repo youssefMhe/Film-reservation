ad7c1dcbf8f1aa51901b617e0dcbaadf5e0d2017
unknown <youssef.mhenni1@gmail.com>
2023-01-08 12:08:15 +0100

    adding model Admin

---
+++ backend/models/Admin.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const adminSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    addedMovies: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Movie"
+    }]
+
+})
+export default mongoose.model('Admin', adminSchema)
