18171948e0fb103adc99015b7a501b3c576b5a0a
unknown <youssef.mhenni1@gmail.com>
2023-01-08 12:10:19 +0100

    Booking Model

---
+++ backend/models/Booking.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema({
+    movie: {
+        type: mongoose.Types.ObjectId,
+        ref: "Movie",
+        required: true
+    }
+    ,
+    date: {
+        type: Date,
+        required: true,
+    },
+    seatNumber: {
+        type: Number,
+        required: true,
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+
+
+})
+export default mongoose.model('Booking', bookingSchema)
